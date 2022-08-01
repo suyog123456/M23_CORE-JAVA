75713389eb0823963c84181ded50c10a09b58b68
HP <HP@LAPTOP-LRS23TOA>
2022-08-01 19:32:07 +0530

    First Commit

--- Day-1_Git and GitHub/src/com/capgemini/github/Hello.java
+++ Day-1_Git and GitHub/src/com/capgemini/github/Hello.java
@@ -3,7 +3,7 @@ package com.capgemini.github;
 public class Hello {
 // to print line
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello World");
 
 	}
 
