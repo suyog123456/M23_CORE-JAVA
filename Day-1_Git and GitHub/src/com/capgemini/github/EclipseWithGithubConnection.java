2745a2f75a16e1f4a817e5ea93e21da5b296c2d5
HP <HP@LAPTOP-LRS23TOA>
2022-08-01 19:14:34 +0530

    First commit

---
+++ Day-1_Git and GitHub/src/com/capgemini/github/EclipseWithGithubConnection.java
@@ -0,0 +1,10 @@
+package com.capgemini.github;
+// Demo for to connect eclipse with github
+public class EclipseWithGithubConnection {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Capgemini C2TC program");	
+
+	}
+
+}
